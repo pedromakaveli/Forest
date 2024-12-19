854410a7ba4c5ad78538aa193b44460f19692778
pedromakaveli <pedrolacerda804@gmail.com>
2024-12-19 19:00:49 -0300

    Inicia repositorio com estrutura do projeto e configuração do tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: ".5rem",
+        sm: "1rem"
+      }
+    },
+    extend: {
+      verde: {
+        200: "#ACEF75",
+        300: "#91EE77",
+        400: "#17E880",
+        700: "#2E482C",
+        800: "#16281F",
+        900: "#0F1C15",
+        950: "#030504",
+
+      }
+    },
+  },
+  plugins: [],
+}
+
